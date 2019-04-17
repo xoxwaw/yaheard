faacb28e5f99457a10a8456a209f3465bbd8a4eb
hungphi98 <nguyenhungphi2698@Gmail.com>
2019-04-17 14:42:05 -0400

    user profile partly done

--- components/views/ProfileView.js
+++ components/views/ProfileView.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, AsyncStorage } from 'react-native';
 import { SwitchNavigator } from 'react-navigation'
+import firebase from 'react-native-firebase';
 
 export default class Profile extends React.Component {
+    constructor(){
+        super();
+        this.ref = firebase.firestore().collection('users');
+        this.storage = firebase.storage();
+        this._retrieveData()
+        this.state = {username: "", karma: 1, recent_posts : []};
+    }
+    _retrieveData = async () => {
+          try {
+              const value = await AsyncStorage.getItem('user');
+              if (value !== null) {
+                  // We have data!!
+                  var query = this.ref.where("email", "==", value);
+                  query.onSnapshot((snapshot) =>{
+                      snapshot.forEach(child => {
+                          const {email, karma, username} = child.data()
+                          this.setState({username: username, karma: karma});
+                      })
+                  })
+
+              }
+          } catch (error) {
+              alert(error);
+              // Error retrieving data
+          }
+    };
+
   render() {
     return (
       <View style={{ flex: 1, flexDirection: 'column' }}>
         <Text>PROFILE</Text>
+        <Text>Hello, {this.state.username}</Text>
+        <Text>Karma: {this.state.karma}</Text>
       </View>
     );
   }
