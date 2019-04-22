c2a564d754324dddc439a31b6ea272a34190c11c
probably-not-porter <pelibby16@earlham.edu>
2019-04-21 22:15:34 -0400

    clean some imports out of finished components

--- components/Main.js
+++ components/Main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import { createSwitchNavigator, withNavigation  } from 'react-navigation'
 
 
