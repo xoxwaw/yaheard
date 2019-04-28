47b8b82b5581b0ee9dffbc48a9cf81b429f3bb69
probably-not-porter <pelibby16@earlham.edu>
2019-04-28 17:47:42 -0400

    add radio buttons fuck that other shit

--- components/views/SettingsView.js
+++ components/views/SettingsView.js
@@ -1,38 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, Picker } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import { SwitchNavigator } from 'react-navigation';
 import firebase from 'react-native-firebase';
 
 const client = require('./Backend/Client');
 
 export default class Create extends React.Component {
-    state = {sort: '', inc: ''}
+    state = {sort: ['gray','gray','gray'], inc: ['gray','gray']}
 
-    updateSort = (sort) => {
-      this.setState({ sort: sort })
-      if (this.state.sort == 'popular'){
-            // if feed should be sorted by popular
-            client.sortByPopular()
-      }
-      else{
-            // if feed should be sorted by new
-      }
-    }
-    updateInc = (inc) => {
-        this.setState({ inc: inc })
-        if (this.state.inc == 'day'){
-            // if feed should be constrained to day
-        }
-        else if(this.state.inc == 'month'){
-            // if feed should be constrained to month
-        }
-        else if (this.state.inc == 'year'){
-            // if feed should be constrained to year
-        }
-        else{
-            // if feed is not constrained
-        }
-      }
+    
   logOut = () => {
     firebase.auth().signOut().then(function() {
       Alert.alert(
@@ -47,28 +23,98 @@ export default class Create extends React.Component {
       alert('error');
     });
   }
+  textStyle = function(ind, group){
+    var color = '';
+    if (group == 'inc')
+    {
+        color = this.state.inc[ind];
+    }
+    else{
+        color = this.state.sort[ind];
+    }
+    if (color == '#4C9A2A'){
+        return {
+            width: '100%', 
+            textAlign: 'center', 
+            fontFamily: 'Pacifico-Bold', 
+            fontSize: 20,
+            color: color,
+            textShadowColor: '#4C9A2A',
+            textShadowOffset: {width: -1, height: 1},
+            textShadowRadius: 10
+        }
+    }
+    else{
+        return {
+            width: '100%', 
+            textAlign: 'center', 
+            fontFamily: 'Pacifico-Bold', 
+            fontSize: 20,
+            color: color,
+        }
+    }
+    
+  }
+  sortDay = () => {
+    this.setState({ sort: ['#4C9A2A','gray','gray'] })
+    // sort to day
+  }
+  sortMonth = () => {
+    this.setState({ sort: ['gray','#4C9A2A','gray'] })
+    // sort to month
+  }
+  sortYear = () => {
+    this.setState({ sort: ['gray','gray','#4C9A2A'] })
+    // sort to year
+  }
+  sortNew = () => {
+    this.setState({ inc: ['#4C9A2A','gray'] })
+    // sort to NEW
+  }
+  sortPop = () => {
+    this.setState({ inc: ['gray','#4C9A2A'] })
+    // sort to POPULAR
+  }
   render() {
     return (
         <View style={{ flex: 1, flexDirection: 'column'}}>
             <Text style={{ fontFamily: 'Pacifico-Bold', fontSize: 22, color: 'black', textAlign: 'center', width: '100%', backgroundColor: '#ccc', height: 40 }}>Settings</Text>
                 
-
-                <View style={{ padding: 10, margin: 10, flex: 1, backgroundColor: '#ddd', borderRadius: 8, marginTop: 40}}>
-                    <Picker selectedValue = {this.state.sort} onValueChange = {this.updateSort}>
-                        <Picker.Item label = "Most Popular" value = "popular" />
-                        <Picker.Item label = "Most Recent" value = "new" />
-                    </Picker>
+            <Text style={{ fontSize: 16, width: '100%', backgroundColor: '#ccc', marginLeft: 20, color: 'gray' }}>See posts from the last:</Text>
+            <View style={{ padding: 10, margin: 10, flex: 1}}>
+                <View style={{ flexDirection: 'row', elevation: 5 }}>
+                    <TouchableOpacity onPress={this.sortDay} style={{ flex: 1, padding: 10, backgroundColor:'#eee', borderTopLeftRadius: 8, borderBottomLeftRadius: 8, borderColor: '#4C9A2A', borderWidth: 0.5 }}>
+                        <Text color={this.state.sort[0]} style={this.textStyle(0, 'sort')}>
+                            Day
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={this.sortMonth} style={{ flex: 1, padding: 10, backgroundColor:'#eee', borderColor: '#4C9A2A', borderWidth: 0.5 }}>
+                        <Text color={this.state.sort[1]} style={this.textStyle(1, 'sort')}>
+                            Month
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={this.sortYear} style={{ flex: 1, padding: 10, backgroundColor:'#eee', borderTopRightRadius: 8, borderBottomRightRadius: 8, borderColor: '#4C9A2A', borderWidth: 0.5 }}>
+                        <Text color={this.state.sort[2]} style={this.textStyle(2, 'sort')}>
+                            Year
+                        </Text>
+                    </TouchableOpacity>
                 </View>
-
-                <View style={{ padding: 10, margin: 10, flex: 1, backgroundColor: '#ddd', borderRadius: 8, marginTop: 40}}>
-                    <Picker selectedValue = {this.state.inc} onValueChange = {this.updateInc}>
-                        <Picker.Item label = "Last day" value = "day" />
-                        <Picker.Item label = "Last Month" value = "month" />
-                        <Picker.Item label = "Last Year" value = "year" />
-                        <Picker.Item label = "All Time" value = "all" />
-                    </Picker>
+            </View>
+            <Text style={{ fontSize: 16, width: '100%', backgroundColor: '#ccc', marginLeft: 20, color: 'gray' }}>Sort posts by:</Text>
+            <View style={{ padding: 10, margin: 10, flex: 1}}>
+                <View style={{ flexDirection: 'row', elevation: 5 }}>
+                    <TouchableOpacity onPress={this.sortNew} style={{ flex: 1, padding: 10, backgroundColor:'#eee', borderTopLeftRadius: 8, borderBottomLeftRadius: 8, borderColor: '#4C9A2A', borderWidth: 0.5 }}>
+                        <Text color={this.state.sort[0]} style={this.textStyle(0, 'inc')}>
+                            Popular
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={this.sortPop} style={{ flex: 1, padding: 10, backgroundColor:'#eee', borderTopRightRadius: 8, borderBottomRightRadius: 8, borderColor: '#4C9A2A', borderWidth: 0.5 }}>
+                        <Text color={this.state.sort[0]} style={this.textStyle(1, 'inc')}>
+                            Recent
+                        </Text>
+                    </TouchableOpacity>
                 </View>
-
+            </View>
 
             <View style={{ padding: 10, flex: 1 }}>
                 <Button
@@ -79,7 +125,7 @@ export default class Create extends React.Component {
                     accessibilityLabel="Log out of YaHeard"
                 />
             </View>
-            <View style={{ flex: 1 }}>
+            <View style={{ flex: 5 }}>
                 
             </View>
 
